8b1c667cc23f40e9fed98ed28e653357b1b22cc2
AlfredoJi <mralfredojimenez1996@gmail.com>
2023-03-07 21:11:04 -0500

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
-
 var bobApi = "https://bobsburgers-api.herokuapp.com/burgerOfTheDay/";
-var endcreditsApi = "https://bobsburgers-api.herokuapp.com/endCreditsSequence/?episode=2&season=1";
-var imdbApi = "https://imdb-api.com/en/API/SeasonEpisodes/k_h9zw3q8g/tt0411008/1";
+var endcreditsApi = "https://bobsburgers-api.herokuapp.com/endCreditsSequence/";
+//var imdbApi = "https://imdb-api.com/en/API/SeasonEpisodes/k_h9zw3q8g/tt0411008/1";
 
 
 $("select").formSelect();
@@ -9,8 +8,17 @@ $("select").formSelect();
 $("#go-button").click(function () {
     console.log($("#dropdown1").val());
     console.log($("#dropdown2").val());
+
+    var season = $("#dropdown1").val();
+    var episode = $("#dropdown2").val();
+    var burgerUrl = bobApi + "?season=" + season + "&episode=" + episode;
+    var creditsUrl = endcreditsApi + "?season=" + season + "&episode=" + episode;
+
+    console.log(burgerUrl);
+    console.log(creditsUrl);
+
     $("p").remove();
-    $("#info-picture").remove();
+    //$("#info-picture").remove();
     var burgerPara = document.createElement("p");
     var textBurger = document.createTextNode("Burger of the day: ");
     burgerPara.setAttribute("id", "info-text");
@@ -29,14 +37,13 @@ $("#go-button").click(function () {
     ratingPara.appendChild(textRating);
     document.getElementById("info").appendChild(ratingPara);
 
-    fetch(bobApi)
+    fetch(burgerUrl)
         .then(function (respone) {
             return respone.json();
         })
         .then(function (data) {
             console.log(data);
             for (var i = 0; i < data.length; i++) {
-                if (data[i].season == $("#dropdown1").val() && data[i].episode == $("#dropdown2").val()) {
                     var burgerName = data[i].name + "  ";
                     var burgerPrice = data[i].price + "  ";
 
@@ -48,26 +55,20 @@ $("#go-button").click(function () {
 
                     console.log(burgerName);
                     console.log(burgerPrice);
-                }
             }
 
         })
 
-
-    fetch(endcreditsApi)
+    fetch(creditsUrl)
         .then(function (respone) {
             return respone.json();
         })
         .then(function (data) {
-            var endcreditsImage = data[0].image;
+            console.log(data[0].image);
+            var creditsImg = data[0].image.split("/revision")[0];
+            console.log(creditsImg);
+            $("#info-picture").attr("src", creditsImg);
         })
 
-    fetch(imdbApi)
-        .then(function (respone) {
-            return respone.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
 
 });
